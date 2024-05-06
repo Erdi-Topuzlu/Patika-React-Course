71b8d1debf82cf215780b083f28e7650153ce5b6
Erdi Topuzlu <erditpzl@gmail.com>
2024-05-07 02:52:42 +0300

    Rendering in Component

--- first-react-app-with-vite/src/App.jsx
+++ first-react-app-with-vite/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Header from './components/Header'
 
+const name = "Erdi";
+const surname = "Topuzlu";
+const isLoggedIn = false;
+
 function App() {
 
   return (
     <>
       <Header />
+      <h1>{isLoggedIn ? name + " " + surname : "Giriş Yapın!"}</h1>
     </>
   )
 }
