5e946862ccd8c6b140ff4c776d61a71155827b76
Erdi Topuzlu <erditpzl@gmail.com>
2024-05-07 15:09:48 +0300

    Array and Object States Usage

--- states-in-react/src/App.jsx
+++ states-in-react/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
 
   const [name, setName] = useState("Erdi");
   const [age, setAge] = useState(32);
+  const [friends, setFriends] = useState(["Fırat", "Onur"]);
+  const [address, setAddress] = useState({city:"İstanbul", zip:34315});
 
   return (
     <>
@@ -12,6 +14,23 @@ function App() {
       <h2>{age}</h2>
       <button onClick={() => {setName("Serhan");}}>Change Name</button>
       <button onClick={() => {setAge(33);}}>Change Age</button>
+
+      <hr />
+      <br />
+      <h2>Friends</h2>
+      {friends.map((friend, index) => (
+        <div key={index}>{friend}</div>
+      ))}
+      
+      <button onClick={() => {setFriends([...friends, "Erkut"]);}}>Add New Friend</button>
+
+      <hr />
+      <br />
+      <h2>Address</h2>
+      <div>{address.city} {address.zip}</div>
+
+      <button onClick={() => {setAddress({...address, city:"Ankara"});}}>Add New Friend</button>
+
     </>
   )
 }
