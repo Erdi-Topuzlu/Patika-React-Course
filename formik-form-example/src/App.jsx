19bb040083c8acf6cbcd9dece9106c1e51272c55
Erdi Topuzlu <erditpzl@gmail.com>
2024-06-03 18:31:33 +0300

    Formik handleChange and handleSubmit Usage

---
+++ formik-form-example/src/App.jsx
@@ -0,0 +1,47 @@
+import "./App.css";
+import { Formik } from "formik";
+
+function App() {
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={(values) => {
+
+          console.log(values);
+        }}
+      >
+        {
+          ( {handleSubmit, handleChange} ) => (
+            <form onSubmit={handleSubmit}>
+          <label htmlFor="firstName">First Name</label>
+          <input name="firstName" onChange={handleChange} />
+          <br />
+          <br />
+
+
+          <label htmlFor="lastName">Last Name</label>
+          <input name="lastName" onChange={handleChange} />
+          <br />
+          <br />
+
+          <label htmlFor="email">Email</label>
+          <input name="email" onChange={handleChange} />
+          <br />
+          <br />
+
+          <button type="submit">Submit</button>
+        </form>
+          )
+        }
+      </Formik>
+    </>
+  );
+}
+
+export default App;
