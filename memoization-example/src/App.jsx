add865b4e920093f6e9ad69f888ffd395ceef139
Erdi Topuzlu <erditpzl@gmail.com>
2024-06-03 22:00:03 +0300

    Usage React.memo

--- memoization-example/src/App.jsx
+++ memoization-example/src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
+  const [number, setNumber] = useState(0);
 
   return (
     <>
-      
+      <Header />
+      <hr />
+      <h1>{number}</h1>
+      <button onClick={() => setNumber(number + 1)}>Click</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ memoization-example/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    console.log("Header Component Re-rendered");
+  return (
+    <div>
+      Header
+    </div>
+  )
+}
+
+export default React.memo(Header);
